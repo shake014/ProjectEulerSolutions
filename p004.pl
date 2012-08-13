a746021ab652bdbcc23829d09e328c07da580d55
shake014 <shake014@umn.edu>
2012-08-13 18:02:10 -0500

    Replace Arrays with Numbers

--- p004.pl
+++ p004.pl
@@ -1,18 +1,28 @@
 #!/usr/bin/perl -w
 use 5.010;
 use bignum;
-@nums1=(100..999);
-@nums2=(100..999);
+use strict;
+use warnings;
+
+sub palindromic{
+	my ($a)=shift;
+	my $ra = reverse $a;
+	if ($a==$ra){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+my @nums1=(100..999);
+my @nums2=(100..999);
+my $num1;
+my $num2;
+my $max = 0;
 foreach $num1 (@nums1){
 	foreach $num2 (@nums2){
 		$a=$num1*$num2;
-		my @nums=split('',$a);
-		my @nums2=reverse(@nums);
-		push(@bignums,$a) if @nums~~@nums2;
+		$max=$a if palindromic($a) && $a>$max;
 	}
 }
-$c=0;
-foreach (@bignums){
-	$c=$_ if $_>=$c;
-}
-print $c;
+print "PE 4:", $max, "\n";
