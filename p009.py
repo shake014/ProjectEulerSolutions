431a5f72aff67ff29359414ae2d540d5348117d8
Sammy Shaker <shake014@umn.edu>
2012-08-14 14:45:23 -0500

    Problems in Perl

--- p009.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-for a in range(1, 500):
-    for b in range(1, 500):
-        for c in range(1, 500):
-            if(a*a+b*b==c*c):
-                if(a+b+c==1000):
-                    print a*b*c
-                    break
