2b3f237f98785095b77785609961a1700efcc888
shake014 <shake014@umn.edu>
2012-08-14 14:48:48 -0500

    Cleaned up variables

--- p001.pl
+++ p001.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl -w
 #Using a for loop and an if conditional statement, add up all the multiples of 3,5 under 1000:
-@nums=(1..1000);
-foreach(@nums){
-    $sum+=$_ if ($_ % 3==0 || $_ % 5==0);
+use strict;
+use warnings;
+my $sum = 0;
+my @nums=(1..1000);
+my $a;
+foreach $a (@nums){
+    $sum+=$a if ($a % 3==0 || $a % 5==0);
 }
 print $sum;
