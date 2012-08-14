6b206ba558d639406337a68060ec1542e40b698e
shake014 <shake014@umn.edu>
2012-08-14 14:52:30 -0500

    New and Improved Solution

--- p005.pl
+++ p005.pl
@@ -1,27 +1,18 @@
 #!/usr/bin/perl -w
 use bignum;
-$m=1;
-$a=2;
-$a**=5;
-$b=3;
-$b**=3;
-$c=5;
-$c**=2;
-$d=7;
-$d**=2;
-foreach(1..20){
-	$m*=$_;
+use strict;
+use warnings;
+
+my $m=2520;
+sub divis{
+	my($m)=shift;
+	my $n;
+	foreach $n (11..20){
+		until($m%$n==0){
+			$m++;
+		}
+	}
+	return $m;
 }
-until($m % $a != 0 ){
-	$m /= 2;
-}
-until($m % $b != 0){
-	$m /= 3;
-}
-until($m % $c != 0){
-	$m /= 5;
-}
-until($m % $d !=0){
-	$m /= 7;
-}
-print $m;
+
+print "PE 5:", divis($m),"\n";
