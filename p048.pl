3db09a601e6de6499a3a5e6d1d154228df11718f
shake014 <shake014@umn.edu>
2012-08-14 14:47:46 -0500

    Cleaned up variables

--- p048.pl
+++ p048.pl
@@ -1,10 +1,13 @@
-$max_n = 1000;
-$m = 1e10;
-$sum = 0;
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+my $max_n = 1000;
+my $m = 1e10;
+my $sum = 0;
 for $n (1..$max_n){
-$nx = 1;
-$nx = ($nx * $n) % $m  for (1..$n); 
-$sum = ($sum + $nx) % $m;
+	my $nx = 1;
+	$nx = ($nx * $n) % $m  for (1..$n); 
+	$sum = ($sum + $nx) % $m;
 }
  
 print "PE 48 = $sum";
