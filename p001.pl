76e9a9ec4ffc79c395e7099b17e1f3cd8201db0b
shake014 <shake014@umn.edu>
2012-08-21 17:46:58 -0500

    Made it more modular

--- p001.pl
+++ p001.pl
@@ -2,10 +2,16 @@
 #Using a for loop and an if conditional statement, add up all the multiples of 3,5 under 1000:
 use strict;
 use warnings;
-my $sum = 0;
-my @nums=(1..1000);
-my $a;
-foreach $a (@nums){
-    $sum+=$a if ($a % 3==0 || $a % 5==0);
+
+sub divby3o5{
+    my(@nums)=@_;
+    my $sum = 0;
+    my $a;
+    foreach $a (@nums){
+        $sum+=$a if ($a%3==0)||($a%5==0)
+    }
+    return $sum
 }
-print $sum;
+
+my @nums=(1..1000);
+print divby3o5(@nums);
