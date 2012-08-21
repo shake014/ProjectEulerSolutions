f92f330838465f95b66de98d5d677f5be23370cf
shake014 <shake014@umn.edu>
2012-08-21 17:53:16 -0500

    Made more modular

--- p002.pl
+++ p002.pl
@@ -1,12 +1,24 @@
 #!/usr/bin/perl -w
+use strict;
+use warnings;
+
+sub even{
+	my($k)=shift;
+	$k % 2 ==0 ? return 1 : return 0
+}
+
+sub fibonnaci{
+	my($i, $j) = @_;
+	my $m = $j + $i;
+	return $m;
+}
+	
 my $i = 1;
 my $j = 1;
-my $k;
 my $sum = 0;
 while($k<4000000){
-	$sum+=$k if $k % 2 == 0;
-	$k = $i + $j;
-	$i = $j;
-	$j = $k;
+	$k = fibonnaci($i, $j);
+	$sum+=$k if even($k);
+	($i, $j) = ($j, $k)
 }
 print "PE 2:", $sum, "\n";
